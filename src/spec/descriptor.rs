87ff3c27c1cb0e5215e690815967c2a6fdc779b5
Benny Zlotnik <bzlotnik@protonmail.com>
2021-02-13 16:01:00 +0200

    some progress on descriptor

--- src/spec/descriptor.rs
+++ src/spec/descriptor.rs
@@ -1,17 +1,22 @@
 use std::collections::HashMap;
+use serde::{Serialize, Deserialize};
+
 use super::media_types::MediaType;
 use super::digest::Digest;
 
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct Descriptor {
     pub media_type: MediaType,
     pub digest: Digest,
     pub size: u64,
-    pub urls: Vec<String>,
-    pub annotations: HashMap<String, String>,
+    pub urls: Option<Vec<String>>,
+    pub annotations: Option<HashMap<String, String>>,
     pub platform: Option<Platform>,
     pub data: Option<String>,
 }
 
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct Platform {
 	// Architecture field specifies the CPU architecture, for example
 	// `amd64` or `ppc64`.
@@ -31,4 +36,35 @@ pub struct Platform {
 	// Variant is an optional field specifying a variant of the CPU, for
 	// example `v7` to specify ARMv7 when architecture is `arm`.
 	variant: String
+}
+
+mod tests {
+    use crate::spec::digest::Algorithm;
+
+    use super::*;
+
+    #[test]
+    fn test_deseriaize_descriptor() {
+        const DESCRIPTOR_JSON: &str = r#"
+        {
+            "mediaType": "application/vnd.oci.image.manifest.v1+json",
+            "size": 7682,
+            "digest": "sha256:5b0bcabd1ed22e9fb1310cf6c2dec7cdef19f0ad69efa1f392e94a4333501270",
+            "urls": [
+              "https://example.com/example-manifest"
+            ]
+        }"#;
+        let descriptor: Descriptor = serde_json::from_str(DESCRIPTOR_JSON).unwrap();
+        let expected = Descriptor {
+            media_type: MediaType::ImageManifest,
+            digest: Digest::new(Algorithm::Sha256, String::from("5b0bcabd1ed22e9fb1310cf6c2dec7cdef19f0ad69efa1f392e94a4333501270")),
+            annotations: None,
+            size: 7682,
+            urls: Some(vec![String::from("https://example.com/example-manifest")]),
+            platform: None,
+            data: None,
+        };
+
+        assert_eq!(descriptor, expected)
+    }
 }
--- src/spec/digest.rs
+++ src/spec/digest.rs
@@ -1,29 +1,70 @@
-use anyhow::{anyhow, Error, Result};
-use sha2::{Sha256, Sha512};
+use std::{fmt, str::FromStr};
 
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+use anyhow::Result;
+use serde::{Deserialize, Deserializer, Serialize};
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub enum Algorithm {
     Sha256,
     Sha512,
     Unregistered(String),
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+impl fmt::Display for Algorithm {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Algorithm::Sha256 => write!(f, "sha256"),
+            Algorithm::Sha512 => write!(f, "sha512"),
+            Algorithm::Unregistered(s) => write!(f, "{}", s),
+        }
+    }
+}
+
+impl FromStr for Algorithm {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "sha256" => Ok(Algorithm::Sha256),
+            "sha512" => Ok(Algorithm::Sha512),
+            _ => Ok(Algorithm::Unregistered(s.to_owned())),
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize)]
 pub struct Digest {
     algorithm: Algorithm,
     encoded: String,
 }
 
 impl Digest {
-
     pub fn new(algorithm: Algorithm, encoded: String) -> Self {
-        Self {
-            algorithm,
-            encoded,
-        }
+        Self { algorithm, encoded }
     }
 
     pub fn validate(&self) -> Result<()> {
-        Ok(())
+        todo!()
+    }
+}
+
+impl fmt::Display for Digest {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}:{}", self.algorithm, self.encoded)
+    }
+}
+
+impl<'de> Deserialize<'de> for Digest {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let s = String::deserialize(deserializer)?;
+        let split: Vec<&str> = s.split(':').collect();
+
+        return Ok(Digest::new(
+            Algorithm::from_str(split[0]).unwrap(),
+            split[1].to_owned(),
+        ));
     }
 }
