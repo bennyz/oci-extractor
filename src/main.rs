99ad10faf98fe90eee6e843bf608e7ab06af5125
Benny Zlotnik <bzlotnik@protonmail.com>
2021-02-19 13:19:24 +0200

    switch to clap

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,34 @@
-use argh::FromArgs;
-
-#[derive(FromArgs, PartialEq, Debug)]
-/// Arguments represents the options umoci-rs accepts
-struct Arguments {
-    /// whether or not to jump
-    #[argh(subcommand)]
-    command: Command,
-}
-#[derive(FromArgs, PartialEq, Debug)]
-#[argh(subcommand)]
-/// Enum represnting available commands
-pub enum Command {
-    Unpack(UnpackCommand),
-}
-
-#[derive(FromArgs, PartialEq, Debug)]
-#[argh(subcommand, name = "unpack")]
-/// Unpacks an OCI image
-pub struct UnpackCommand {
-    #[argh(option)]
-    /// how many x
-    x: usize,
-}
+use clap::{App, Arg, SubCommand};
 
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("umoci-rs")
+        .version("0.1")
+        .author("Benny Zlotnik <bzlotnik@protonmail.com>")
+        .about("umoci-rs currently unpacks OCI images")
+        .subcommand(
+            SubCommand::with_name("unpack")
+                .about("unpacks OCI image")
+                .arg(
+                    Arg::with_name("image")
+                        .long("image")
+                        .required(true)
+                        .takes_value(true)
+                        .help("Image name to unapck in the format: <image-path>[:<tag>]"),
+                )
+                .arg(
+                    Arg::with_name("bundle")
+                        .takes_value(true)
+                        .required(true)
+                        .help("path of the bundle"),
+                ),
+        )
+        .get_matches();
+
+    if let Some(unpack) = matches.subcommand_matches("unpack") {
+        // Use the struct like normal
+        let image = unpack.value_of("image").unwrap();
+        let bundle = unpack.value_of("bundle").unwrap();
+        println!("image: {}", image);
+        println!("bundle: {}", bundle);
+    }
 }
