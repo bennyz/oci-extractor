83ed172616e19bbb7f71f918936c826e5d4e62ad
Benny Zlotnik <bzlotnik@protonmail.com>
2021-11-06 17:47:10 +0200

    upgrade clap

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,30 @@
-use clap::{App, Arg, SubCommand};
+use clap::{AppSettings, Parser};
 use umoci_rs::unpacker::Unpacker;
 
+#[derive(Parser)]
+struct Opts {
+    #[clap(subcommand)]
+    subcmd: SubCommand,
+}
+
+#[derive(Parser)]
+enum SubCommand {
+    Unpack(Unpack),
+}
+
+#[derive(Parser)]
+struct Unpack {
+    #[clap(long)]
+    image: String,
+    destination: String,
+}
+
 fn main() {
-    let matches = App::new("umoci-rs")
-        .version("0.1")
-        .author("Benny Zlotnik <bzlotnik@protonmail.com>")
-        .about("umoci-rs currently unpacks OCI images")
-        .subcommand(
-            SubCommand::with_name("unpack")
-                .about("unpacks OCI image")
-                .arg(
-                    Arg::with_name("image")
-                        .long("image")
-                        .required(true)
-                        .takes_value(true)
-                        .help("Image name to unapck in the format: <image-path>[:<tag>]"),
-                )
-                .arg(
-                    Arg::with_name("destination")
-                        .takes_value(true)
-                        .required(true)
-                        .help("rootfs target dir"),
-                ),
-        )
-        .get_matches();
-
-    if let Some(unpack) = matches.subcommand_matches("unpack") {
-        // Use a struct like normal person
-        let image = unpack.value_of("image").unwrap();
-        let destination = unpack.value_of("destination").unwrap();
-
-        let u = Unpacker::new(String::from(image), String::from(destination));
-
-        u.unpack();
+    let opts: Opts = Opts::parse();
+    match opts.subcmd {
+        SubCommand::Unpack(u) => {
+            let unpacker = Unpacker::new(u.image, u.destination);
+            unpacker.unpack();
+        }
     }
 }
